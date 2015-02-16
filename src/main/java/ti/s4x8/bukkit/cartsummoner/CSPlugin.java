8aa1003226acdbc5654bb4e9bb294caf9a37890a
Marcos Vives Del Sol <socram8888@gmail.com>
2015-02-16 16:41:59 +0100

    Screw the configuration signs! And the permissions!

--- src/main/java/ti/s4x8/bukkit/cartsummoner/CSPlugin.java
+++ src/main/java/ti/s4x8/bukkit/cartsummoner/CSPlugin.java
@@ -1,27 +1,12 @@
 
 package ti.s4x8.bukkit.cartsummoner;
 
-import org.bukkit.entity.Player;
-import org.bukkit.entity.minecart.RideableMinecart;
-import org.bukkit.permissions.Permission;
-import org.bukkit.permissions.PermissionDefault;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.plugin.PluginManager;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import lombok.Getter;
 
 public class CSPlugin extends JavaPlugin {
 	public static final String CARTSUMMON_SPAWNED_META = "cartsummon.spawned";
 
-	@Getter private Permission createPermission = new Permission("cartsummoner.create", "Allows creation of cart summoners", PermissionDefault.OP);
-	@Getter private Permission summonPermission = new Permission("cartsummoner.summon", "Allows to summon a cart", PermissionDefault.TRUE);
-	@Getter private Permission seeFailPermission = new Permission("cartsummoner.see-fail", "Player can see fail messages", PermissionDefault.TRUE);
-
 	public void onEnable() {
-		new SignCreationListener(this);
 		new CartSummonListener(this);
 		new CartLeaveListener(this);
 	}
--- src/main/java/ti/s4x8/bukkit/cartsummoner/CartSummonListener.java
+++ src/main/java/ti/s4x8/bukkit/cartsummoner/CartSummonListener.java
@@ -3,8 +3,9 @@ package ti.s4x8.bukkit.cartsummoner;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
-import org.bukkit.World;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.minecart.RideableMinecart;
@@ -13,9 +14,13 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.material.Button;
 import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.util.Vector;
 
 public class CartSummonListener implements Listener {
+	private final Vector BLOCK_CENTER = new Vector(0.5, 0.5, 0.5);
+
 	private CSPlugin plugin;
 
 	public CartSummonListener(CSPlugin plugin) {
@@ -33,26 +38,15 @@ public class CartSummonListener implements Listener {
 			return;
 		}
 
-		CartSign sign;
 		Block buttonBlock = event.getClickedBlock();
-		try {
-			sign = CartSign.fromButton(buttonBlock);
-		} catch (InvalidCartSignException e) {
+		Location cartLocation = calculateCartLocation(buttonBlock);
+		if (cartLocation == null) {
 			return;
 		}
 
-		if (!player.hasPermission(plugin.getSummonPermission())) {
-			if (player.hasPermission(plugin.getSeeFailPermission())) {
-				player.sendMessage(ChatColor.RED + "You don't have permission to summon a cart");
-			}
-
-			return;
-		}
-
-		World world = buttonBlock.getWorld();
-		RideableMinecart cart = (RideableMinecart) world.spawnEntity(sign.getAbsoluteLocation(), EntityType.MINECART);
+		RideableMinecart cart = (RideableMinecart) cartLocation.getWorld().spawnEntity(cartLocation, EntityType.MINECART);
 		if (cart == null) {
-			player.sendMessage(ChatColor.RED + "Plugin was unable to spawn a cart");
+			player.sendMessage(ChatColor.RED + "Plugin has been unable to spawn a cart");
 			return;
 		}
 
@@ -61,5 +55,49 @@ public class CartSummonListener implements Listener {
 		cart.setPassenger(player);
 
 		player.sendMessage(ChatColor.GREEN + "Here's your cart. Have fun!");
+	}
+
+	/*
+	This function checks if the blocks the button is facing to matches this pattern
+	┌───────┬───────┐
+	│  ???  │ SAFE  │
+	│       │  AIR  │
+	├───────┼───────┤
+	│ BUT-  │ SAFE  │
+	│ TON   │  AIR  │
+	├───────┼───────┤
+	│ REDS- │ POWR. │
+	│ TONE  │ RAIL  │
+	└───────┴───────┘
+	Check MaterialFamily.SAFE_AIR_BLOCKS for blocks I've considered as "safe". They're all either non-solid blocks, or blocks with a very small bounding box.
+	*/
+	private Location calculateCartLocation(Block buttonBlock) {
+		if (!MaterialFamily.BUTTONS.hasMaterial(buttonBlock)) {
+			return null;
+		}
+
+		if (buttonBlock.getRelative(BlockFace.DOWN).getType() != Material.REDSTONE_WIRE) {
+			return null;
+		}
+
+		Button button = (Button) buttonBlock.getState().getData();
+		BlockFace buttonFacing = button.getFacing();
+
+		Block airBlock1 = buttonBlock.getRelative(buttonFacing);
+		if (!MaterialFamily.SAFE_AIR_BLOCKS.hasMaterial(airBlock1)) {
+			return null;
+		}
+
+		Block airBlock2 = airBlock1.getRelative(BlockFace.UP);
+		if (!MaterialFamily.SAFE_AIR_BLOCKS.hasMaterial(airBlock2)) {
+			return null;
+		}
+
+		Block railBlock = airBlock1.getRelative(BlockFace.DOWN);
+		if (railBlock.getType() != Material.POWERED_RAIL) {
+			return null;
+		}
+
+		return railBlock.getLocation().add(BLOCK_CENTER);
 	}
 }
